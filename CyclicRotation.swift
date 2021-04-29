1a2e447394558f3d33c69ecc4ea9d27473f61c0d
irnafitriani <irna.fitriani@gmail.com>
2021-04-29 17:11:10 +0700

    Create  CyclicRotation.swift

---
+++ CyclicRotation.swift
@@ -0,0 +1,29 @@
+
+
+public func solution(_ A : inout [Int], _ K : Int) -> [Int] {
+    // write your code in Swift 4.2.1 (Linux)
+    var array = A
+    var rotate = K
+
+    if A.count == 0 {
+        return []
+    }
+
+    if A.count == 1 {
+        return array
+    }
+
+    if K > A.count {
+        rotate = K - A.count
+    }
+
+    for i in 0...A.count-1 {
+        let swap = i + rotate
+        if swap > A.count-1 {
+            array[swap - A.count] = A[i]
+        } else {
+            array[swap] = A[i]
+        }
+    }
+    return array
+}
